8be9ffaed0ec5fff6e50e8c6410dba9a96080499
alaiam <alaia.morell@gmail.com>
2024-12-11 11:47:54 -0800

    Improve the data processing when the model does not finish the simulation + improve the data processing when fishery = F

--- R/demo.R
+++ R/demo.R
@@ -6,10 +6,11 @@
 # bio.prm = "C:/Users/Alaia/Desktop/Postdoc/R script/atlantis2ls/data/AMPSbioparam_mv1_2024_V4.prm"
 # bio.prm2 = "C:/Users/Alaia/Desktop/Postdoc/R script/atlantis2ls/data/AMPSbioparam_mv1_20222.prm"
 # bio.lines <- readLines(bio.prm)
-# a <- read_atlantis(path = path, prefix = prefix, fg.file = fg.file, N_only = T)
+# a <- read_atlantis(path = path, prefix = prefix, fg.file = fg.file, N_only = F)
 # outputs.nc <- ncdf4::nc_open("C:/Users/Alaia/Desktop/Postdoc/R script/atlantis2ls/data/AMPS_OUT.nc")
-
-
+# atlantis_sf %>% ungroup %>% select(area,box_id )
+# atlantis_bgm <- read_bgm(paste(path,"/PugetSound_89b_070116.bgm", sep = ""))
+# atlantis_bgm$boxes$area
 # for (i in 1:10){
 #   edit_param_mum_age(bio.prm=bio.prm,
 #                      new.mum = get_param_mum_age(bio.prm, write.output = F, output.dir, out.name, vector.size = i)*2,
--- R/read_catch.R
+++ R/read_catch.R
@@ -6,16 +6,18 @@
 #' @param prefix
 #' @param fg.file
 #' @param fishery
+#' @param dt.timeserie
 #'
 #' @return
 #' @export
 #'
 #' @examples
-extract_catch_main <- function(path, prefix = NULL, fg.file, fishery = F){
+extract_catch_main <- function(path, prefix = NULL, fg.file, fishery = F, dt.timeserie){
   fg <- process_fg(fg.file)
 
   if (fishery == F){
-    catch <- rep(list(rep(0,75)),length(fg$Name))
+    fg <- process_fg(fg.file)
+    catch <- rep(list(rep(0,dt.timeserie)),length(fg$Name))
     names(catch) <- fg$Name
   }else{
 
